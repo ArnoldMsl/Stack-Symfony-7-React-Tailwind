538c2c464966c0f282d9f127f57a507ef10fc2bc
ArnoldMsl <arnold.masselin@gmail.com>
2024-06-27 10:08:43 +0200

    homepage

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,4 @@ import './bootstrap.js';
 import './styles/output.css';
 import { registerReactControllerComponents } from '@symfony/ux-react';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
-
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
-registerReactControllerComponents();
--- assets/react/controllers/Hello.jsx
+++ assets/react/controllers/Hello.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function (props) {
-    return <div>Hello {props.fullName}</div>;
+    return <div>Hello ! I am a React component and I was developped by {props.name} </div>;
 }
